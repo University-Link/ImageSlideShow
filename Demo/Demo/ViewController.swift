15e56c9e67b0369fd071f9631a687c5b63869409
Seungsoo Lee <few280@gmail.com>
2021-11-04 23:55:33 +0900

    chore: Change code style

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -10,97 +10,77 @@ import UIKit
 
 //	Very bad Class, but just for Demo ;-)
 
-class Image: NSObject, ImageSlideShowProtocol
-{
-	private let url: URL
-	let title: String?
-	
-	init(title: String, url: URL) {
-		self.title = title
-		self.url = url
-	}
-	
-	func slideIdentifier() -> String {
-		return String(describing: url)
-	}
-	
-	func image(completion: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
-		
-		let session = URLSession(configuration: URLSessionConfiguration.default)
-		session.dataTask(with: self.url) { data, response, error in
-			
-			if let data = data, error == nil
-			{
-				let image = UIImage(data: data)
-				completion(image, nil)
-			}
-			else
-			{
-				completion(nil, error)
-			}
-			
-		}.resume()
-		
-	}
+class Image: NSObject, ImageSlideShowProtocol {
+  
+  private let url: URL
+  let title: String?
+  
+  init(title: String, url: URL) {
+    self.title = title
+    self.url = url
+  }
+  
+  func slideIdentifier() -> String {
+    return String(describing: url)
+  }
+  
+  func image(completion: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
+    let session = URLSession(configuration: URLSessionConfiguration.default)
+    session.dataTask(with: self.url) { data, response, error in
+      if let data = data, error == nil {
+        let image = UIImage(data: data)
+        completion(image, nil)
+      } else {
+        completion(nil, error)
+      }
+    }.resume()
+  }
 }
 
 class ViewController: UIViewController {
-
-	fileprivate var images:[Image] = []
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		self.generateImages()
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
-	fileprivate func generateImages()
-	{
-		let scale:Int = Int(UIScreen.main.scale)
-		let height:Int = Int(view.frame.size.height) * scale
-		let width:Int = Int(view.frame.size.width) * scale
-		
-		images = [
-			Image(title: "Image 1", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09a/fff.png&text=Image+1")!),
-			Image(title: "Image 2", url: URL(string: "https://dummyimage.com/\(600)x\(600)/09b/fff.png&text=Image+2")!),
-			Image(title: "Image 3", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09c/fff.png&text=Image+3")!),
-			Image(title: "Image 4", url: URL(string: "https://dummyimage.com/\(600)x\(600)/09d/fff.png&text=Image+4")!),
-			Image(title: "Image 5", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09e/fff.png&text=Image+5")!),
-			Image(title: "Image 6", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09f/fff.png&text=Image+6")!),
-		]
-	}
-	
-	@IBAction func presentSlideShow(_ sender:AnyObject?)
-	{
-		ImageSlideShowViewController.presentFrom(self){ [weak self] controller in
-			
-			controller.dismissOnPanGesture = true
-			controller.slides = self?.images
-			controller.enableZoom = true
-			controller.controllerDidDismiss = {
-				debugPrint("Controller Dismissed")
-				
-				debugPrint("last index viewed: \(controller.currentIndex)")
-			}
-			
-			controller.slideShowViewDidLoad = {
-				debugPrint("Did Load")
-			}
-			
-			controller.slideShowViewWillAppear = { animated in
-				debugPrint("Will Appear Animated: \(animated)")
-			}
-			
-			controller.slideShowViewDidAppear = { animated in
-				debugPrint("Did Appear Animated: \(animated)")
-			}
-			
-		}
-	}
+  
+  fileprivate var images:[Image] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.generateImages()
+  }
+  
+  fileprivate func generateImages() {
+    let scale:Int = Int(UIScreen.main.scale)
+    let height:Int = Int(view.frame.size.height) * scale
+    let width:Int = Int(view.frame.size.width) * scale
+    images = [
+      Image(title: "Image 1", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09a/fff.png&text=Image+1")!),
+      Image(title: "Image 2", url: URL(string: "https://dummyimage.com/\(600)x\(600)/09b/fff.png&text=Image+2")!),
+      Image(title: "Image 3", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09c/fff.png&text=Image+3")!),
+      Image(title: "Image 4", url: URL(string: "https://dummyimage.com/\(600)x\(600)/09d/fff.png&text=Image+4")!),
+      Image(title: "Image 5", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09e/fff.png&text=Image+5")!),
+      Image(title: "Image 6", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09f/fff.png&text=Image+6")!),
+    ]
+  }
+  
+  @IBAction func presentSlideShow(_ sender:AnyObject?) {
+    ImageSlideShowViewController.presentFrom(self){ [weak self] controller in
+      
+      controller.dismissOnPanGesture = true
+      controller.slides = self?.images
+      controller.enableZoom = true
+      controller.controllerDidDismiss = {
+        debugPrint("Controller Dismissed")
+        debugPrint("last index viewed: \(controller.currentIndex)")
+      }
+      
+      controller.slideShowViewDidLoad = {
+        debugPrint("Did Load")
+      }
+      controller.slideShowViewWillAppear = { animated in
+        debugPrint("Will Appear Animated: \(animated)")
+      }
+      controller.slideShowViewDidAppear = { animated in
+        debugPrint("Did Appear Animated: \(animated)")
+      }
+    }
+  }
 }
 
